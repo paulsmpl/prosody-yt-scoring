36a811e3559df511755c8a494da23441571010fd
Paul <paul@simplicia.co>
2026-02-08 14:33:06 +0100

    Fix HTTPS bootstrap without cert

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -6,6 +6,8 @@ REPO_URL="https://github.com/paulsmpl/prosody-yt-scoring.git"
 APP_DIR="$HOME/prosody-yt-scoring"
 NGINX_AVAILABLE="/etc/nginx/sites-available/${DOMAIN}"
 NGINX_ENABLED="/etc/nginx/sites-enabled/${DOMAIN}"
+NGINX_HTTP_CONF="deploy/nginx-prosody-checker.http.conf"
+NGINX_SSL_CONF="deploy/nginx-prosody-checker.conf"
 
 sudo apt update
 sudo apt install -y git nginx certbot python3-certbot-nginx
@@ -24,7 +26,7 @@ git pull
 
 docker compose up -d --build
 
-sudo cp deploy/nginx-prosody-checker.conf "$NGINX_AVAILABLE"
+sudo cp "$NGINX_HTTP_CONF" "$NGINX_AVAILABLE"
 if [ ! -L "$NGINX_ENABLED" ]; then
   sudo ln -s "$NGINX_AVAILABLE" "$NGINX_ENABLED"
 fi
@@ -35,6 +37,10 @@ sudo systemctl restart nginx
 if [ ! -d "/etc/letsencrypt/live/${DOMAIN}" ]; then
   sudo certbot --nginx -d "$DOMAIN"
 fi
+
+sudo cp "$NGINX_SSL_CONF" "$NGINX_AVAILABLE"
+sudo nginx -t
+sudo systemctl restart nginx
 
 sudo systemctl restart nginx
 
